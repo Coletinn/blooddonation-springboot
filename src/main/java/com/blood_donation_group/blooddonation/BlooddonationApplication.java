1065dfdbf7181ecfee11affe39fc183cdc70dfac
Coletinn <gustavocoleto@hotmail.com>
2022-09-28 11:26:59 -0300

    Backend criado

---
+++ src/main/java/com/blood_donation_group/blooddonation/BlooddonationApplication.java
@@ -0,0 +1,13 @@
+package com.blood_donation_group.blooddonation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlooddonationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlooddonationApplication.class, args);
+	}
+
+}
